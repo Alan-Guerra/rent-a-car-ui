d557d87232c0afdc62c29df393746502c127b8f1
AGuerra <alanguerra.war@gmail.com>
2024-03-02 18:33:10 -0300

    Implement react router

---
+++ rent-a-car-ui/src/components/Rent.tsx
@@ -0,0 +1,3 @@
+export const Rent = () => {
+    return <div>RENT</div>
+}
--- rent-a-car-ui/src/main.tsx
+++ rent-a-car-ui/src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { Catalog } from './components/Catalog.tsx'
+
+const router = createBrowserRouter([
+  {
+    element: <Catalog />,
+    path: "/catalog",
+  },
+  {
+    element: <App />,
+    path: "/",
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
+  
 )
