0487120a6da1afc34262095170bb29c8c8ca7548
AGuerra <alanguerra.war@gmail.com>
2024-03-02 20:44:16 -0300

    Add Logo component in Sidebar

--- rent-a-car-ui/src/components/Sidebar.tsx
+++ rent-a-car-ui/src/components/Sidebar.tsx
@@ -1,7 +1,11 @@
+import Logo from "./Logo"
+
 export default function Sidebar() {
     return (
         <div className="bg-blue-800 flex-1 max-w-80">
-            
+            <div className="flex py-4 px-10">
+                <Logo />
+            </div>
         </div>
     )
 }
