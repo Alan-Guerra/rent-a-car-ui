db3d12ff3d226196869cd946b56632d4571efafa
AGuerra <alanguerra.war@gmail.com>
2024-03-04 09:00:58 -0300

    Add tests to sidebar links

--- rent-a-car-ui/src/routes/Catalog.tsx
+++ rent-a-car-ui/src/routes/Catalog.tsx
@@ -3,7 +3,7 @@ import Main from "../components/Main"
 
 export const Catalog = () => {
     return (
-        <div data-cy="catalog" className="flex flex-1 h-full w-full">
+        <div data-cy="catalogPage" className="flex flex-1 h-full w-full">
             <Sidebar />
             <Main />
         </div>
